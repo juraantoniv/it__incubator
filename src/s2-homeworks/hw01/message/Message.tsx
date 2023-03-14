fd624d0689f50ddd9d6c9400364aee5503a4f2a9
juraantoniv <juraantoniv@gmail.com>
2023-03-14 16:19:46 +0100

    Initial commit

---
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import s from './Message.module.css'
+import {message0, MessageType} from "../HW1";
+
+// нужно создать правильный тип вместо any
+export type MessagePropsType = {
+    message: MessageType
+}
+
+// нужно отобразить приходящие данные
+const Message = (props: MessagePropsType) => {
+
+    return (
+        <div id={'hw1-message-' + props.message.id} className={s.message}>
+            <div className={s.imageAndText}>
+               <div className={s.in}>
+                <img
+                    id={'hw1-avatar-' + props.message.id}
+                    src={props.message.user.avatar}
+
+                />
+                <div className={s.text}>
+                    <div id={'hw1-name-' + props.message.id} className={s.name}>
+                        <div>{props.message.user.name}</div>
+                    </div>
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                         <div>{props.message.message.text}</div>
+                    </pre>
+                </div>
+                </div>
+            </div>
+            <div id={'hw1-time-' + props.message.id} className={s.time}>
+                <div>{props.message.message.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
---
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -0,0 +1,85 @@
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import Greeting from './Greeting'
+import { UserType } from './HW3'
+import user from "../hw08/User";
+
+type GreetingContainerPropsType = {
+    users: Array<UserType> // need to fix any
+    addUserCallback: (name:string)=>void // need to fix any
+}
+
+export const pureAddUser = (name:string, setError: (str:string)=>void, setName: (st:string)=>void, addUserCallback: (name:string)=>void ) => {
+
+    if (name){
+
+        setName(name)
+        addUserCallback(name)
+
+    }
+    else {
+        setError('err')
+    }
+
+}
+
+export const pureOnBlur = (name: string, setError: any) => {
+    // если имя пустое - показать ошибку
+    if (!name) setError('Field is required')
+}
+
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: any) => {
+    if (e.key==='Enter'){
+        addUser()
+    }
+    // если нажата кнопка Enter - добавить
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+    users,
+    addUserCallback,
+}) => {
+    // деструктуризация пропсов
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+
+    const setNameCallback = (e:ChangeEvent<HTMLInputElement>) => {
+
+        setName(e.currentTarget.value)// need to fix
+        error && setError('')
+    }
+
+
+    const addUser = () => {
+        pureAddUser(name, setError, setName, addUserCallback)
+    }
+
+    const onBlur = () => {
+        pureOnBlur(name, setError)
+    }
+
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        pureOnEnter(e, addUser)
+    }
+
+    const totalUsers = users.length > 0 ? users.length : 0 // need to fix
+    const lastUserName = users.length > 0 ? users[users.length-1].name : '' // need to fix
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            onBlur={onBlur}
+            onEnter={onEnter}
+            error={error}
+            totalUsers={totalUsers}
+            lastUserName={lastUserName}
+        />
+    )
+}
+
+export default GreetingContainer
